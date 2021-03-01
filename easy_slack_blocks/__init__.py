26fddbd79b55d70ed65851f9cbb00076577a1d56
Matthew Cles <matthew@cles.dev>
2021-03-01 18:19:45 +0800

    update init

--- easy_slack_blocks/__init__.py
+++ easy_slack_blocks/__init__.py
@@ -1,11 +1,1 @@
 from .block_builder import BlockBuilder
-from .components import (
-    Confirmation,
-    DispatchActionConfiguration,
-    Filter,
-    Option,
-    OptionGroup,
-    Text,
-    Button,
-    Image,
-)
