5efe5af4a6cf606f07b33b5ab29f9281124ebfee
Matthew Cles <matthew@cles.dev>
2021-03-01 13:58:21 +0800

    move OptionGroup class to its own file

--- easy-slack-blocks/components/option.py
+++ easy-slack-blocks/components/option.py
@@ -37,28 +37,3 @@ class Option(dict):
         ### add element validation
 
         super(Option, self).__init__(option)
-
-class OptionGroup(dict):
-    """Slack Option composition builder.
-
-    For more information, see the following URL:
-    https://api.slack.com/reference/block-kit/composition-objects#option_group
-    """
-    def __init__(
-        self,
-        label,
-        options,
-    ):
-        if isinstance(label, str):
-            label = Text(label, Text.PLAIN_TEXT, emoji=False)
-
-        if not isinstance(options, list):
-            options = [options]
-
-        ### TODO:
-        ### add element validation
-
-        super(OptionGroup, self).__init__(
-            label=label,
-            options=options
-        )
