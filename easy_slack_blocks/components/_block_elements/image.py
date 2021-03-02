be8154aee0245330db00b108f2acbe860739c8a4
Matthew Cles <matthew@cles.dev>
2021-03-02 19:59:43 +0800

    update docstring

--- easy_slack_blocks/components/_block_elements/image.py
+++ easy_slack_blocks/components/_block_elements/image.py
@@ -1,13 +1,13 @@
 class Image(dict):
-    """docstring for Image.
+    """Generate an Image element.
 
-    See https://api.slack.com/reference/block-kit/block-elements#image
-    for more information.
+    See Slack documentation for the Image Block Element here:
+    https://api.slack.com/reference/block-kit/block-elements#image
     """
-    def __init__(self, image_url, text_description):
+    def __init__(self, image_url, alt_text):
         super(Image, self).__init__(
             type='image', 
             image_url=image_url, 
-            alt_text=text_description,
+            alt_text=alt_text,
         )
         
