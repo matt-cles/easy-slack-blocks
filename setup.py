455ace19313214b10e6339245087d3c9c747da9a
Matthew Cles <matthew@cles.dev>
2021-03-02 23:15:54 +0800

    increment version for initial pypi publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme_file:
 
 setuptools.setup(
     name='easy-slack-blocks',
-    version='0.0.1',
+    version='1.0.0',
     author='Matthew Cles',
     author_email='matthew@cles.dev',
     description='An easy way to format Slack Blocks.',
