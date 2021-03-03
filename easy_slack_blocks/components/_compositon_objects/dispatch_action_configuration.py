cdf4d8049f28573d29b283e59579070076ec0504
Matthew Cles <matthew@cles.dev>
2021-03-03 15:26:19 +0800

    add validation flag

--- easy_slack_blocks/components/_compositon_objects/dispatch_action_configuration.py
+++ easy_slack_blocks/components/_compositon_objects/dispatch_action_configuration.py
@@ -12,25 +12,28 @@ class DispatchActionConfiguration(object):
         ON_CHARACTER_ENTERED,
     ]
 
-    def __init__(self, trigger_actions_on):
+    def __init__(self, trigger_actions_on, *, validate=True):
         if not isinstance(trigger_actions_on, list):
             trigger_actions_on = [trigger_actions_on]
         
-        if (
-            len(trigger_actions_on) < 1 or 
-            len(trigger_actions_on) > len(ALL_DISPATCH_ACTION_CONFIGURATIONS)
-        ):
-            raise ValueError(
-                'trigger_actions_on should be one or both of: '
-                f'{ON_ENTER_PRESSED}, {ON_CHARACTER_ENTERED}'
-            )
 
-        for action in trigger_actions_on:
-            if action not in ALL_DISPATCH_ACTION_CONFIGURATIONS:
+        if validate:
+            max_len = len(ALL_DISPATCH_ACTION_CONFIGURATIONS)
+            if (
+                len(trigger_actions_on) < 1 or 
+                len(trigger_actions_on) > max_len
+            ):
                 raise ValueError(
                     'trigger_actions_on should be one or both of: '
                     f'{ON_ENTER_PRESSED}, {ON_CHARACTER_ENTERED}'
                 )
+
+            for action in trigger_actions_on:
+                if action not in ALL_DISPATCH_ACTION_CONFIGURATIONS:
+                    raise ValueError(
+                        'trigger_actions_on should be one or both of: '
+                        f'{ON_ENTER_PRESSED}, {ON_CHARACTER_ENTERED}'
+                    )
 
         super(DispatchActionConfiguration, self).__init__(
             trigger_actions_on=trigger_actions_on,
