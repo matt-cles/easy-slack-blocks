dca6f4c9fc1461971e0d8d3562d3f2ceba7fece2
Matthew Cles <matthew@cles.dev>
2021-03-04 18:46:42 +0800

    rework text block validation and make it optional

--- easy_slack_blocks/components/_compositon_objects/text.py
+++ easy_slack_blocks/components/_compositon_objects/text.py
@@ -7,37 +7,55 @@ class Text(dict):
     MRKDWN = MARKDOWN = RICH = 'mrkdwn'
     PLAIN_TEXT = PLAIN = 'plain_text'
 
-    def __init__(self, text, text_type='mrkdwn', *, emoji=None, verbatim=None):
-        if text_type == Text.MRKDWN:
-            if emoji is not None:
+    def __init__(
+        self, 
+        text, 
+        text_type='mrkdwn', 
+        *, 
+        emoji=None, 
+        verbatim=None,
+        validate=True,
+    ):
+        text_dict = {
+            'text': text,
+            'type': text_type,
+        }
+
+        if emoji:
+            text_dict['emoji'] = emoji
+
+        if verbatim:
+            text_dict['verbatim'] = verbatim
+
+        if validate:
+            if not isinstance(text, str):
+                raise ValueError('The \'text\' parameter must be a String')
+
+            if text_type == Text.MRKDWN:
+                if emoji is not None:
+                    raise ValueError(
+                        'The \'emoji\' parameter is only usable when '
+                        '\'text_type\' is \'plain_text\'.'
+                    )
+                if verbatim is not None and not isinstance(verbatim, bool):
+                    raise ValueError(
+                        'The \'verbatim\' parameter must be of type Boolean.'
+                    )
+
+            elif text_type == Text.PLAIN_TEXT:
+                if verbatim is not None:
+                    raise ValueError(
+                        'The \'verbatim\' parameter is only usable when '
+                        '\'text_type\' is \'mrkdwn\'.'
+                    )
+                if emoji is not None and not isinstance(emoji, bool):
+                    raise ValueError(
+                        'The \'emoji\' parameter must be of type Boolean.'
+                    )
+            else:
                 raise ValueError(
-                    'The \'emoji\' parameter is only usable when '
-                    '\'text_type\' is \'plain_text\'.'
+                    'The \'text_type\' parameter must either be \'mrkdwn\' or '
+                    '\'plain_text\''
                 )
-            # Set default 'verbatim' value if none was passed
-            if verbatim is None:
-                verbatim = False
-            super(Text, self).__init__(
-                text=text, 
-                type=text_type, 
-                verbatim=verbatim,
-            )
-        elif text_type == Text.PLAIN_TEXT:
-            if verbatim is not None:
-                raise ValueError(
-                    'The \'verbatim\' parameter is only usable when '
-                    '\'text_type\' is \'mrkdwn\'.'
-                )
-            # Set default 'emoji' value if none was passed
-            if emoji is None:
-                emoji = True
-            super(Text, self).__init__(
-                text=text,
-                type=text_type,
-                emoji=emoji,
-            )
-        else:
-            raise ValueError(
-                'The \'text_type\' parameter must either be \'mrkdwn\' or '
-                '\'plain_text\''
-            )
+
+        super(Text, self).__init__(text_dict)
