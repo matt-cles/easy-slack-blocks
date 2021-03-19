ddbeb71a30178b4fdea00703829f092af665b2f3
Matthew Cles <matthew@cles.dev>
2021-03-19 15:04:58 +0800

    add optional validation

--- easy_slack_blocks/components/_compositon_objects/filter.py
+++ easy_slack_blocks/components/_compositon_objects/filter.py
@@ -2,7 +2,9 @@ class Filter(object):
     """Slack Filter composition object builder.
 
     For more information, see the following URL:
-    https://api.slack.com/reference/block-kit/composition-objects#filter_conversations"""
+    https://api.slack.com/reference/block-kit/composition-objects#
+    filter_conversations
+    """
 
     PUBLIC = 'public'
     PRIVATE = 'private'
@@ -14,6 +16,8 @@ class Filter(object):
         include=None,
         exclude_external_shared_channels=False,
         exclude_bot_users=False,
+        *,
+        validate=True,
     ):
         if include is None:
             include = [
@@ -26,34 +30,36 @@ class Filter(object):
         if not isinstance(include, list):
             include = [include]
 
-        for element in include:
-            if element not in [PUBLIC, PRIVATE, IM, MPIM]:
-                raise ValueError(
-                    '\'include\' must be an array of strings from the '
-                    'following options: im, mpim, private, and public\n'
-                    'see https://api.slack.com/reference/block-kit/'
-                    'composition-objects#filter_conversations for more '
-                    'information'
-                )
-
-        if not isinstance(exclude_external_shared_channels, bool):
-            raise ValueError(
-                '\'exclude_external_shared_channels\' must be a True or '
-                'False value\nsee https://api.slack.com/reference/block-kit/'
-                'composition-objects#filter_conversations for more '
-                'information'
-            )
-
-        if not isinstance(exclude_bot_users, bool):
-            raise ValueError(
-                '\'exclude_bot_users\' must be a True or False value\nsee '
-                'https://api.slack.com/reference/block-kit/composition-'
-                'objects#filter_conversations for more information'
-            )
-
         super(Filter, self).__init__(
             include=include,
             exclude_external_shared_channels=exclude_external_shared_channels,
             exclude_bot_users=exclude_bot_users,
         )
+
+        if validate:
+            self.validate()
         
+    def validate(self):
+        for element in self.get('include'):
+            if element not in [PUBLIC, PRIVATE, IM, MPIM]:
+                raise ValueError(
+                    '\'include\' must be an array of strings from the '
+                    'following options: im, mpim, private, and public\n'
+                    'see https://api.slack.com/reference/block-kit/'
+                    'composition-objects#filter_conversations__fields for more '
+                    'information'
+                )
+
+        if not isinstance(exclude_external_shared_channels, bool):
+            raise ValueError(
+                '\'exclude_external_shared_channels\' must be a Boolean.\n'
+                'see https://api.slack.com/reference/block-kit/composition-'
+                'objects#filter_conversations__fields for more information'
+            )
+
+        if not isinstance(exclude_bot_users, bool):
+            raise ValueError(
+                '\'exclude_bot_users\' must be a Boolean.\nsee https://api.'
+                'slack.com/reference/block-kit/composition-objects#'
+                'filter_conversations__fields for more information'
+            )
